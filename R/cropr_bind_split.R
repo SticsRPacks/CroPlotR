84a284cc698535a347810484d7fe9f713378a81b
sbuis <samuel.buis@inra.fr>
2021-03-12 14:42:58 +0100

    Added functions to bind and split CropR formatted simulations

---
+++ R/cropr_bind_split.R
@@ -0,0 +1,67 @@
+#' Bind rows for class Cropr
+#'
+#' @description Bind simulated data of different situations
+#'
+#' @param sim  Simulation outputs in Cropr format (named list of `data.frame` for each situation,
+#' having the attribute cropr_simulation)
+#'
+#' @return A single data.frame or tibble binding the rows of all data.Frames or tibbles included in sim
+#'
+#' @seealso split_df2sim
+#'
+#' @export
+#'
+#' @import dplyr
+#'
+#' @examples
+#' \dontrun{
+#' # Importing an example with three situations with observation:
+#' workspace= system.file(file.path("extdata", "stics_example_1"), package = "CroPlotR")
+#' situations= SticsRFiles::get_usms_list(usm_path = file.path(workspace,"usms.xml"))
+#' sim= SticsRFiles::get_daily_results(workspace = workspace, usm_name = situations)
+#'
+#' bind_rows_sim(sim)
+#' }
+bind_rows_sim <- function(sim){
+  attr(sim,"class")=NULL   # dplyr::bind_rows does not run correctly if attribute is not NULL or if class is a vector of classes ...
+  return(bind_rows(sim, .id="id") %>% rename(situation=id))
+}
+
+
+#' Split data.frame into Cropr format
+#'
+#' @description Split a row-binded data.frame (or tibble) into a Cropr format simulation list.
+#'
+#' @param df  A single data.frame or tibble containing simulation results (as created by `bind_rows_sim`).
+#' MUST include `Date`and `situation` columns.
+#'
+#' @return A named list of `data.frame` for each situation, having the attribute cropr_simulation.
+#'
+#' @seealso bind_rows_sim
+#'
+#' @export
+#'
+#' @import dplyr
+#' @import tibble
+#'
+#' @examples
+#' \dontrun{
+#' # Importing an example with three situations with observation:
+#' workspace= system.file(file.path("extdata", "stics_example_1"), package = "CroPlotR")
+#' situations= SticsRFiles::get_usms_list(usm_path = file.path(workspace,"usms.xml"))
+#' sim= SticsRFiles::get_daily_results(workspace = workspace, usm_name = situations)
+#'
+#' df <- bind_rows_sim(sim)
+#' split_df2sim(df)
+#' }
+split_df2sim <- function(df){
+  sim <- split(df,f=df$situation, drop = TRUE)
+
+  # remove columns full of NA
+  sim <- lapply(sim,function(y) y %>% select(where(function(x) !all(is.na(x))))
+                %>% select(-situation) %>% remove_rownames())
+
+  attr(sim, "class")= "cropr_simulation"
+  return(sim)
+
+}
---
+++ tests/testthat/test-bind_split.R
@@ -0,0 +1,7 @@
+workspace= system.file(file.path("extdata", "stics_example_1"), package = "CroPlotR")
+situations= SticsRFiles::get_usms_list(usm_path = file.path(workspace,"usms.xml"))
+sim= SticsRFiles::get_daily_results(workspace = workspace, usm_name = situations)
+
+test_that("multiplication works", {
+  expect_identical(sim, split_df2sim(bind_rows_sim(sim)))
+})
