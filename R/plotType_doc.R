a3622f929c01f66bac8bfe6a77a395fd9e1e6cc5
Patrick Lutz <contact.patrick.lutz@gmail.com>
2021-06-22 16:55:20 +0200

    additional documentation on plot types added

---
+++ R/plotType_doc.R
@@ -0,0 +1,11 @@
+#' Plot type thickness.mswc.norg
+#'
+#' @param name ?
+#' @param thickness ?
+#' @param mswc ?
+#' @param norg ?
+#' @param max.overlaps ?
+#' @name thickness.mswc.norg
+#'
+NULL
+#> NULL
