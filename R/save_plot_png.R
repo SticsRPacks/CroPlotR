074d66d755e38d704d8e2e5159b5450f2e7ad53c
Michel Giner <michel.giner@cirad.fr>
2022-11-08 11:41:16 +0100

    cran submission

--- R/save_plot_png.R
+++ R/save_plot_png.R
@@ -7,22 +7,27 @@
 #' @param suffix A suffix to append to the file name
 #' @param width  The plot width
 #' @param height The plot heigth
-#' @param units  The units for plot width and heigth in `units ("in", "cm", or "mm")`
+#' @param units  The units for plot width and heigth in
+#' `units ("in", "cm", or "mm")`
 #' @param dpi    The plot resolution.
 #' @param scale  The scaling factor.
-#' @param device Device to use. Can either be a device function (e.g. png()), or one of
-#' "eps", "ps", "tex" (pictex), "pdf", "jpeg", "tiff", "png", "bmp", "svg" or "wmf"
-#' (windows only).
+#' @param device Device to use. Can either be a device function (e.g. png()),
+#'  or one of "eps", "ps", "tex" (pictex), "pdf", "jpeg", "tiff", "png", "bmp",
+#'   "svg" or "wmf" (windows only).
 #' @param path `r lifecycle::badge("deprecated")` `path` is no
 #'   longer supported, use `out_dir` instead.
 #'
 #' @details The function uses [ggplot2::ggsave()] under the hood.
-#' @return Save the plots to `path`, named by the situation name, and returns the plots invisibly for piping.
+#' @return Save the plots to `path`, named by the situation name,
+#' and returns the plots invisibly for piping.
 #' @export
 #'
-save_plot_png <- function(plot, out_dir, suffix = "", width = 17, height = 12, units = "cm", dpi = 200, scale = 1.2, device = NULL, path = lifecycle::deprecated()) {
+save_plot_png <- function(plot, out_dir, suffix = "", width = 17, height = 12,
+                          units = "cm", dpi = 200, scale = 1.2, device = NULL,
+                          path = lifecycle::deprecated()) {
   if (lifecycle::is_present(path)) {
-    lifecycle::deprecate_warn("0.5.0", "save_plot_png(path)", "save_plot_png(out_dir)")
+    lifecycle::deprecate_warn("0.5.0", "save_plot_png(path)",
+                              "save_plot_png(out_dir)")
   } else {
     path <- out_dir # to remove when we update inside the function
   }
