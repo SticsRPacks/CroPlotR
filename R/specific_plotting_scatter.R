558a246c8e70b2619aa6c91845902ed1b2734458
sbuis <samuel.buis@inra.fr>
2024-01-29 14:47:28 +0100

    Added first draft of function plot_scat_mixture_allsit

---
+++ R/specific_plotting_scatter.R
@@ -0,0 +1,102 @@
+
+#' Specific functions to generate scatter plots
+#'
+#' @description Generate scatter plots for the differents cases handled in CroPlotR
+#' (plant mixture, plot of residuals, plot several simulation results on same graph, ...)
+#' as specitifed by the different arguments.
+#'
+#' @inheritParams generic_plotting
+#'
+#' @param df_data A named list of data frame including the data to plot (one df
+#' per situation, or only one df ifsit==all_situations)
+#' @param sit The name of the situation to plot (or all_situations)
+#' @param is_obs_sd TRUE if error standard deviation of observations is provided
+#'
+#' @importFrom rlang .data
+#' @return A ggplot object
+#'
+#' @details List of the different specific functions:
+#' \itemize{
+#'   \item `plot_scat_mixture_allsit`: Generate a scatter plot for the case of
+#' mixture of crops, single simulation version and all_situations in same plot
+#' }
+#'
+#' @return A list of ggplot objects
+#'
+#' @name specific_scatter_plots
+#'
+NULL
+
+
+#' @keywords internal
+#' @rdname specific_scatter_plots
+plot_scat_mixture_allsit <- function(df_data, sit, select_scat, shape_sit,
+                                     reference_var, is_obs_sd) {
+
+  # Different treatments for plotting sim or res
+  ## TODO: should be in different functions ??? See if there are some lines of code in common
+
+  df_data <- df_data %>%
+    dplyr::filter(!is.na(Observed) & !is.na(Simulated))
+
+  if (select_scat == "sim") {
+
+    p <- ggplot2::ggplot(df_data, ggplot2::aes(y = Simulated, x = Observed)) +
+      ggplot2::geom_abline(intercept = 0, slope = 1, color = "grey30", linetype = 2) +
+      ggplot2::geom_point(mapping = ggplot2::aes(x = Simulated, y = Observed), alpha = 0, na.rm = TRUE) +
+      # Invisible points of coordinates (y,x) allowing to have both axes at
+      # the same scale
+      # could be done using ggh4x package ? see https://community.rstudio.com/t/plot-facet-wrap-with-free-scales-but-with-same-limits/147088/4
+      ggplot2::geom_point(ggplot2::aes(x = Simulated, y = Observed), alpha = 0, na.rm = TRUE)
+
+
+  } else if (select_scat == "res") {
+
+    p <- ggplot2::ggplot(df_data, ggplot2::aes(Observed - Simulated, x = Observed)) +
+      ggplot2::geom_abline(intercept = 0, slope = 0, color = "grey30", linetype = 2) +
+    if (!is.null(reference_var)) {
+      p <- p + ggplot2::xlab(reference_var)
+    }
+  }
+
+  p <- p +
+    ggplot2::geom_point(
+      ggplot2::aes(colour = as.factor(paste(Dominance, ":", Plant))),
+      na.rm = TRUE
+    ) +
+    ggplot2::geom_smooth(
+      method = lm, color = "blue",
+      se = FALSE, linewidth = 0.6, formula = y ~ x,
+      fullrange = TRUE, na.rm = TRUE
+    )
+
+  if (is_obs_sd) {
+    p <- p +
+      ggplot2::geom_errorbarh(
+        ggplot2::aes(xmin = Observed - 2 * Obs_SD, xmax = Observed + 2 * Obs_SD),
+        na.rm = TRUE
+      )
+  }
+
+  p <- p +
+    ggplot2::labs(fill = "Plant") +
+    ggplot2::facet_wrap(~ variable, scales = "free") +
+    ggplot2::theme(aspect.ratio = 1)
+
+  if (shape_sit == "txt") {
+    p <- p + ggrepel::geom_text_repel(ggplot2::aes(label = Sit_Name), na.rm = TRUE, show.legend = FALSE, max.overlaps = Inf)
+  }
+
+  if (shape_sit == "txt") {
+    p <- p +
+      ggrepel::geom_text_repel(
+        ggplot2::aes(label = Sit_Name,
+                     colour = as.factor(paste(Dominance, ":", Plant)),
+                     na.rm = TRUE, show.legend = FALSE, max.overlaps = Inf
+        ))
+  }
+
+  p <- p + ggplot2::scale_color_discrete(name = "Plant")
+
+  return(p)
+}
