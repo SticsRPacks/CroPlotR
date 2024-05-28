2427ce18026b42af5f539a63b5b365bf37104f47
Rémi Vezy <VEZY@users.noreply.github.com>
2024-05-28 17:32:18 +0200

    Add plot_dynamic_mixture_overlap

--- R/specific_plotting_dynamic.R
+++ R/specific_plotting_dynamic.R
@@ -1,39 +1,63 @@
 plot_dynamic_mixture <- function(df_data, sit) {
-    ggplot2::ggplot(
-        df_data,
-        ggplot2::aes(x = .data$Date, colour = .data$Plant)
+  ggplot2::ggplot(
+    df_data,
+    ggplot2::aes(x = .data$Date, colour = .data$Plant)
+  ) +
+    ggplot2::geom_line(ggplot2::aes(y = .data$Simulated)) +
+    ggplot2::geom_point(
+      ggplot2::aes(y = .data$Observed, shape = .data$Plant),
+      na.rm = TRUE
     ) +
-        ggplot2::geom_line(ggplot2::aes(y = .data$Simulated)) +
-        ggplot2::geom_point(
-            ggplot2::aes(y = .data$Observed, shape = .data$Plant),
-            na.rm = TRUE
-        ) +
-        ggplot2::facet_wrap(~ .data$variable)
+    ggplot2::facet_wrap(~ .data$variable)
+}
+
+plot_dynamic_mixture_overlap <- function(df_data, sit, overlap) {
+  print("Youhou")
+  ggplot2::ggplot(
+    df_data,
+    ggplot2::aes(
+      x = .data$Date,
+      colour = paste(.data$Dominance, ": ", .data$Plant),
+      linetype = .data$variable, shape = .data$Plant
+    )
+  ) +
+    ggplot2::geom_line(
+      ggplot2::aes(
+        y = .data$Simulated
+      )
+    ) +
+    ggplot2::geom_point(
+      ggplot2::aes(
+        y = .data$Observed
+      ),
+      na.rm = TRUE, show.legend = FALSE
+    ) +
+    ggplot2::facet_wrap(~ .data$group_var, scales = "free") +
+    ggplot2::labs(color = "Plant", linetype = "Variable")
 }
 
 plot_dynamic_versions <- function(df_data, sit) {
-    ggplot2::ggplot(
-        df_data,
-        ggplot2::aes(x = .data$Date, colour = .data$version)
+  ggplot2::ggplot(
+    df_data,
+    ggplot2::aes(x = .data$Date, colour = .data$version)
+  ) +
+    ggplot2::geom_line(ggplot2::aes(y = .data$Simulated)) +
+    ggplot2::geom_point(
+      ggplot2::aes(y = .data$Observed, shape = .data$version),
+      na.rm = TRUE
     ) +
-        ggplot2::geom_line(ggplot2::aes(y = .data$Simulated)) +
-        ggplot2::geom_point(
-            ggplot2::aes(y = .data$Observed, shape = .data$version),
-            na.rm = TRUE
-        ) +
-        ggplot2::facet_wrap(~ .data$variable, scales = "free")
+    ggplot2::facet_wrap(~ .data$variable, scales = "free")
 }
 
-
 plot_dynamic_overlap <- function(df_data, sit) {
-    ggplot2::ggplot(
-        df_data,
-        ggplot2::aes(x = .data$Date, colour = .data$variable)
+  ggplot2::ggplot(
+    df_data,
+    ggplot2::aes(x = .data$Date, colour = .data$variable)
+  ) +
+    ggplot2::geom_line(ggplot2::aes(y = .data$Simulated)) +
+    ggplot2::geom_point(
+      ggplot2::aes(y = .data$Observed),
+      na.rm = TRUE
     ) +
-        ggplot2::geom_line(ggplot2::aes(y = .data$Simulated)) +
-        ggplot2::geom_point(
-            ggplot2::aes(y = .data$Observed),
-            na.rm = TRUE
-        ) +
-        ggplot2::facet_wrap(~ .data$group_var, scales = "free")
+    ggplot2::facet_wrap(~ .data$group_var, scales = "free")
 }
