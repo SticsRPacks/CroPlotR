a15103ba20aa39bf37664abd949d372f07e4f19f
Rémi Vezy <VEZY@users.noreply.github.com>
2024-05-28 14:42:02 +0200

    Parse `plot_situations` arguments in its own function

--- R/generic_plotting.R
+++ R/generic_plotting.R
@@ -71,146 +71,34 @@ plot_situations <- function(..., obs = NULL, obs_sd = NULL,
                             shape_sit = c("none", "txt", "symbol", "group"),
                             situation_group = NULL, reference_var = NULL,
                             force = FALSE, verbose = TRUE) {
-  dot_args <- list(...)
-  type <- match.arg(type, c("dynamic", "scatter"), several.ok = FALSE)
-  select_dyn <- match.arg(select_dyn, c("sim", "common", "obs", "all"),
-    several.ok = FALSE
+  args_list <- parse_plot_situations_args(
+    list(...), obs, obs_sd, type, select_dyn, select_scat, var, title,
+    all_situations, overlap, successive, shape_sit, situation_group,
+    reference_var, force, verbose
   )
 
-  # Now that we have one data.frame only, we can test if we have observations / obs_sd:
-  is_obs <- !is.null(obs) && all(sapply(obs, function(x) nrow(x) > 0))
-  is_obs_sd <- !is.null(obs_sd) && all(sapply(obs, function(x) nrow(x) > 0))
-
-  # Early error on observations (no observations given at all but we need them):
-  # NB: `generic_formatting` will check if there are common observations and simulations
-  if (!is_obs && (type == "scatter" || select_dyn %in% c("common", "obs"))) {
-    if (verbose) {
-      cli::cli_alert_warning(
-        c(
-          "Observations are required but not provided, ",
-          "did you provide `obs = ...`?"
-        )
-      )
-    }
-    if (force) {
-      return(NULL)
-    } else {
-      stop(
-        "Observations are required but not provided, ",
-        "did you provide `obs = ...`?"
-      )
-    }
-  }
-
-  select_scat <- match.arg(select_scat, c("sim", "res"), several.ok = FALSE)
-  shape_sit <- match.arg(shape_sit, c("none", "txt", "symbol", "group"),
-    several.ok = FALSE
-  )
-
-  if (select_scat == "res" || shape_sit != "none") {
-    type <- "scatter"
-  }
-
-  # Disable all_situations when type=="dynamic" temporarily
-  if (type == "dynamic") {
-    all_situations <- FALSE
-  }
-
-  # Enable successive and overlap only when type=="dynamic"
-  if (!is.null(successive) || !is.null(overlap)) {
-    type <- "dynamic"
-  }
-
-  # Text on scatter plots only if all_situations
-  if (shape_sit != "none") {
-    all_situations <- TRUE
-  }
-
-  # Enable reference variable edit only when plotting residual scatter plot
-  if (!is.null(reference_var)) {
-    type <- "scatter"
-    # select_scat <- "res"
-  }
-
-  if (!is.null(situation_group)) {
-    shape_sit <- "group"
-  }
-
-  if (shape_sit == "group" && is.null(situation_group)) {
-    if (verbose) {
-      cli::cli_alert_warning("Argument `situation_group` must be defined
-                             when `shape_sit` is 'group'")
-    }
-    if (force) {
-      return(NULL)
-    } else {
-      stop("Argument `situation_group` not defined. Use `force = TRUE` to avoid this error.")
-    }
-  }
-
-  several_sit <- (all_situations || !is.null(successive)) &&
-    shape_sit %in% c("symbol", "group")
-
-  # Name the models:
-  v_names <- names(dot_args)
-  if (is.null(v_names) || length(v_names) < length(dot_args)) {
-    v_names <- paste0("Version_", seq_along(dot_args))
-    names(dot_args) <- v_names
-  }
-
-  # Don't show group in legend if only one:
-  if (length(v_names) == 1) {
-    one_version <- TRUE
-  } else {
-    one_version <- FALSE
-  }
-
-  # If there are successive situations:
-  if (!is.null(successive)) {
-    # Cat situations that need to be represented as a contiguous sequence(dynamic)
-    list_rot <- cat_successive(dot_args, obs, successive)
-    dot_args <- list_rot[[1]]
-    obs <- list_rot[[2]]
-  }
-
-  # Intersect situations between versions
-  common_situations_models <- names(dot_args[[1]])
-
-  if (length(dot_args) > 1) {
-    for (index in 2:length(dot_args)) {
-      common_situations_models <- intersect(
-        common_situations_models,
-        names(dot_args[[index]])
-      )
-    }
-  }
-
-  if (length(title) == 1) {
-    if (!all_situations) {
-      title <- rep(title, length(common_situations_models))
-      names(title) <- common_situations_models
-    } else {
-      title <- NULL
-    }
-  }
-
-  if (!is.null(title) && length(title) != length(common_situations_models) &&
-    is.null(names(title))) {
-    if (verbose) {
-      cli::cli_alert_danger("Situations number is different from model(s)
-                            outputs, please name the {.code title} argument
-                            with the situations names.")
-    }
-    # Situations number is different from models outputs, can't guess
-    # which title is for which situation.
-    stop("title argument is not a named list")
-  }
-
-  if (!is.null(title) && is.null(names(title))) {
-    # title is provided by the user, is not named, but has same length than
-    # common_situations_models, so we guess it:
-    names(title) <- common_situations_models
-  }
+  dot_args <- args_list$dot_args
+  obs <- args_list$obs
+  obs_sd <- args_list$obs_sd
+  type <- args_list$type
+  select_dyn <- args_list$select_dyn
+  select_scat <- args_list$select_scat
+  var <- args_list$var
+  title <- args_list$title
+  all_situations <- args_list$all_situations
+  overlap <- args_list$overlap
+  successive <- args_list$successive
+  shape_sit <- args_list$shape_sit
+  situation_group <- args_list$situation_group
+  reference_var <- args_list$reference_var
+  force <- args_list$force
+  verbose <- args_list$verbose
+  v_names <- args_list$v_names
+  one_version <- args_list$one_version
+  several_sit <- args_list$several_sit
+  common_situations_models <- args_list$common_situations_models
+  is_obs <- args_list$is_obs
+  is_obs_sd <- args_list$is_obs_sd
 
   # Format the data into a list of situations, with the situation repeated
   # as a column name, and the version also as a column name. In the case
@@ -314,7 +202,7 @@ plot_situations <- function(..., obs = NULL, obs_sd = NULL,
       "mixture_no_versions_overlap" = NA,
       "mixture_no_versions_no_overlap" = plot_dynamic_mixture(sim_situation, i),
       "non_mixture_versions_overlap" = NA,
-      "non_mixture_versions_no_overlap" = plot_dynamic_versions(sim_situation,i),
+      "non_mixture_versions_no_overlap" = plot_dynamic_versions(sim_situation, i),
       "non_mixture_no_versions_overlap" = NA,
       "non_mixture_no_versions_no_overlap" = NA,
 
@@ -325,7 +213,11 @@ plot_situations <- function(..., obs = NULL, obs_sd = NULL,
         plot_scat_mixture_allsit(
           sim_situation, i, select_scat, shape_sit,
           reference_var, is_obs_sd,
-          title = if(i=="all_situations"){NULL}else{i}
+          title = if (i == "all_situations") {
+            NULL
+          } else {
+            i
+          }
         ),
       "non_mixture_versions_situations" = NA,
       "non_mixture_versions_per_situations" = NA,
