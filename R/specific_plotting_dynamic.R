0f2339ad26360562a17964ae83ac819bb3d98227
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2024-05-28 17:15:54 +0200

    update plot dyn overlap

--- R/specific_plotting_dynamic.R
+++ R/specific_plotting_dynamic.R
@@ -24,3 +24,16 @@ plot_dynamic_versions <- function(df_data, sit) {
         ggplot2::facet_wrap(~ .data$variable, scales = "free")
 }
 
+
+plot_dynamic_overlap <- function(df_data, sit) {
+    ggplot2::ggplot(
+        df_data,
+        ggplot2::aes(x = .data$Date, colour = .data$variable)
+    ) +
+        ggplot2::geom_line(ggplot2::aes(y = .data$Simulated)) +
+        ggplot2::geom_point(
+            ggplot2::aes(y = .data$Observed),
+            na.rm = TRUE
+        ) +
+        ggplot2::facet_wrap(~ .data$group_var, scales = "free")
+}
