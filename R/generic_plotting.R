8813070a1854d6e5e1d8746534d8ae5d1b1a16c2
Rémi Vezy <VEZY@users.noreply.github.com>
2025-04-30 16:03:01 +0200

    fix typo

--- R/generic_plotting.R
+++ R/generic_plotting.R
@@ -342,7 +342,7 @@ plot_generic_situation <- function(sim, obs = NULL, obs_sd = NULL,
 #' `all_situations= TRUE`. See details.
 #' @param situation_group A list of lists of situations to gather when
 #' `shape_sit= "group"`.
-#' @param reference_var Variable selected on x-axis when type is scaétter and
+#' @param reference_var Variable selected on x-axis when type is scatter and
 #' select_scat is res. It is possible to select
 #' between observation and simulation of the reference variable.
 #' (examples : reference_var = "lai_n_obs", reference_var = "mafruit_sim")
