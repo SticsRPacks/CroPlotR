43bdaffae0a9ddfca60b9636fff4ed80212f4911
Rémi Vezy <VEZY@users.noreply.github.com>
2025-04-30 17:07:23 +0200

    Update test-scatter-plots.R

--- tests/testthat/test-scatter-plots.R
+++ tests/testthat/test-scatter-plots.R
@@ -231,7 +231,7 @@ tmp$reference_var <- lapply(
 all_plots <- list()
 
 # Test the different variants of plots based on the file _inputs/tests_scatter_plot.csv
-invisible(lapply(1:nrow(tmp), function(i) {
+invisible(lapply(seq_len(nrow(tmp)), function(i) {
   test_that(paste0("Test #", tmp$Number[[i]]), {
     if (tmp$version[i]) {
       test_plot <- plot(tmp$sim[[i]], tmp$sim2[[i]],
